5248ba3db56340d24686790e643afc38f0792239
salvatoluice <salvatoluice5@gmail.com>
2023-03-24 18:46:01 +0300

    Add login form

---
+++ client/src/pages/LoginPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const LoginPage = () => {
+  return (
+    <div className='mt-4 '>
+      <h1 className='text-4xl text-center mb-4'>Login</h1>
+      <form className='max-w-md mx-auto'>
+        <input type="email" placeholder='your@email.com' />
+        <input type="password" placeholder='Password' />
+        <button className='primary'>Login</button>
+      </form>
+    </div>
+  )
+}
+
+export default LoginPage
