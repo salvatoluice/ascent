b952bdcabccf2dddaa3c1a647ec298f33b949ed2
salvatoluice <salvatoluice5@gmail.com>
2023-03-27 23:06:53 +0300

    Set state for form

--- client/src/pages/CarsPage.jsx
+++ client/src/pages/CarsPage.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { Link, useParams } from 'react-router-dom'
+import Perks from '../Perks';
 
 const CarsPage = () => {
     const {action} = useParams();
@@ -8,7 +9,7 @@ const CarsPage = () => {
     const [contaact, setContaact] = useState('');
     const [addedPhotos, setAddedPhotos] = useState([]);
     const [photoLink, setPhotoLink] = useState('');
-    const [descritpion, setDescritpion] = useState('');
+    const [description, setDescription] = useState('');
     const [perks, setPerks] = useState([]);
     const [extraInfo, setExtraInfo] = useState('');
     const [checkin, setCheckin] = useState('');
@@ -48,15 +49,14 @@ const CarsPage = () => {
         <div>
           <form>
             {preInput('Title', 'Title and model of your car. Should be Short and descriptive')}
-            <p className='text-gray-500 text-sm'></p>
-            <input type='text' placeholder='Car Name & Brand' />
+            <input type='text' placeholder='Car Name & Brand' value={title} onChange={e => setTitle(e.target.value)} />
             {preInput('Address', 'Address to your place')}
-            <input type="text" placeholder='Address' />
+            <input type="text" placeholder='Address' value={address} onChange={e => setAddress(e.target.value)} />
             {preInput('Contact', 'Your Mobile Phone Number')}
-            <input type="text" placeholder='Phone number, +254...' />
+            <input type="text" placeholder='Phone number, +254...' value={contaact} onChange={e => setContaact(e.target.value)} />
             {preInput('Photos', 'Upload descriptive  photos kindly')}
             <div className='flex gap-2'>
-              <input type="text" placeholder='Or, Upload using Image Url ...jpg' />
+              <input type="text" placeholder='Or, Upload using Image Url ...jpg' value={photoLink} onChange={e => setPhotoLink(e.target.value)} />
               <button className='bg-gray-200 px-4 rounded-xl'>Add&nbsp; Photo</button>
             </div>
             <div className='mt-2 grid grid-cols-3 md:grid-cols-4 lg:grid-cols-6'>
@@ -68,26 +68,26 @@ const CarsPage = () => {
               </button>
             </div>
             {preInput('Description', 'Provide More Info about this car. Be detailed as possible')}
-            <textarea />
+            <textarea value={description} onChange={e => setDescription(e.target.value)}/>
             {preInput('Perks', 'Select all that apply')}
             <div className='grid grid-cols-2 mt-2 gap-2 md:grid-cols-3 lg:grid-cols-6'>
-             
+             <Perks selected={perks} onChange={setPerks} />
             </div>
             {preInput('Extra Info', 'Car rules and any other vital information')}
-            <textarea />
+            <textarea value={extraInfo} onChange={e => setExtraInfo(e.target.value)} />
             {preInput('Number of Days', 'How many days can your car be rented? Max number of passengers your car can accommodate')}
             <div>
               <div className='grid gap-2 sm:grid-cols-3'>
                 <h3 className='mt-2 -mb-2'>First day available</h3>
-                <input type="text" placeholder='31/03/2023 19:00...' />
+                <input type="text" placeholder='31/03/2023 19:00...' value={checkin} onChange={e => setCheckin(e.target.value)} />
               </div>
               <div>
                 <h3>Last day/Return date</h3>
-                <input type="text" placeholder='31/03/2023 19:00...' />
+                <input type="text" placeholder='31/03/2023 19:00...' value={checkout} onChange={e => setCheckout(e.target.value)} />
               </div>
               <div>
                 <h3>Number of passengers</h3>
-                <input type="text" placeholder='Passengers'/>
+                <input type="text" placeholder='Passengers' value={maxPass} onChange={e => setMaxPass(e.target.value)} />
               </div>
             </div>
             <button className='primary my-4'>Save</button>
