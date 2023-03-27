8ed074b15c19a91c791c12d5b0afed8ead79f9d7
salvatoluice <salvatoluice5@gmail.com>
2023-03-27 11:28:02 +0300

    Logout UI

--- client/src/pages/Account.jsx
+++ client/src/pages/Account.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react'
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { UserContext } from '../UserContext'
+import axios from 'axios';
 
 const Account = () => {
     const {user} = useContext(UserContext);
@@ -10,6 +11,12 @@ const Account = () => {
     if (subpage === undefined) {
         subpage = 'profile';
       }
+
+      async function logout() {
+        await axios.post('/logout')
+      }
+
+
     if (!user) {
         return 'Loading';
     }
@@ -29,7 +36,8 @@ const Account = () => {
 
 
   return (
-    <nav className="w-full flex justify-center mt-8 gap-2 mb-8">
+    <>
+        <nav className="w-full flex justify-center mt-8 gap-2 mb-8">
       <Link className={linkClasses('profile')} to={'/account'}>
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
           <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
@@ -49,6 +57,13 @@ const Account = () => {
         My Cars
       </Link>
     </nav>
+         {subpage === 'profile' && (
+            <div className='text-center max-w-lg mx-auto'>
+                Logged in as: <br /> Name: {user.name} <br />Email: {user.email} <br />
+                <button onClick={logout} className='primary max-w-sm mt-2'>Logout</button>
+            </div>
+        )}
+    </>
   )
 }
 
