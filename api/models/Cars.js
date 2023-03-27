99a40059e54f3b73be677867b4a2c968690f4473
salvatoluice <salvatoluice5@gmail.com>
2023-03-27 12:13:52 +0300

    Add id of car  owner

--- api/models/Cars.js
+++ api/models/Cars.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 const carSchema = new mongoose.Schema({
+    owner: mongoose.Schema.Types.ObjectId,
     title: String,
     address: String,
     photos: [String],
