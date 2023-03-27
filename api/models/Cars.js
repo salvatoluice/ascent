ecf30a3a517398579ccde783d0113165d3bb4316
salvatoluice <salvatoluice5@gmail.com>
2023-03-27 12:12:02 +0300

    Car model and schema

---
+++ api/models/Cars.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const carSchema = new mongoose.Schema({
+    title: String,
+    address: String,
+    photos: [String],
+    description: String,
+    perks: [String],
+    extraInfo: String,
+    checkIn: Number,
+    checkout: Number,
+    maxPass: Number
+});
+
+const CarModel = mongoose.model('Car', carSchema);
+
+module.exports = CarModel;
