5d576c739e165f851d7c3b773127907cccb31bef
salvatoluice <salvatoluice5@gmail.com>
2023-03-30 16:54:44 +0300

    Component refactor

--- client/src/pages/CarsPage.jsx
+++ client/src/pages/CarsPage.jsx
@@ -1,61 +1,8 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import Perks from '../Perks';
-import axios from 'axios';
-import PhotoUploader from './PhotoUploader';
+import NewCarForm from './NewCarForm';
 
 const CarsPage = () => {
-    const [title, setTitle] = useState('');
-    const [address, setAddress] = useState('');
-    const [contaact, setContaact] = useState('');
-    const [addedPhotos, setAddedPhotos] = useState([]);
-    const [description, setDescription] = useState('');
-    const [perks, setPerks] = useState([]);
-    const [extraInfo, setExtraInfo] = useState('');
-    const [checkin, setCheckin] = useState('');
-    const [checkout, setCheckout] = useState('');
-    const [maxPass, setMaxPass] = useState(1);
-
-    const navigate = useNavigate();
-
-    function inputHeader(text) {
-      return (
-        <h2 className="text-2xl mt-4">{text}</h2>
-      );
-    }
-    function inputDescription(text) {
-      return (
-        <p className="text-gray-500 text-sm">{text}</p>
-      );
-    }
-    function preInput(header,description) {
-      return (
-        <>
-          {inputHeader(header)}
-          {inputDescription(description)}
-        </>
-      );
-    }
-
-    async function addNewCar(e){
-      e.preventDefault();
-
-      const carData = {
-        title, address, contaact,
-        addedPhotos, description, perks, 
-        extraInfo, checkin, checkout, 
-        maxPass
-      };
-      await axios.post('/cars', {
-        title, address, contaact,
-        addedPhotos, description, perks, 
-        extraInfo, checkin, checkout, 
-        maxPass
-      });
-      navigate('/account')
-    }
-
-
     // console.log(action);
   return (
     <div>
@@ -67,42 +14,7 @@ const CarsPage = () => {
                 Add New Car
             </Link>
         </div>
-        <div>
-          <form onSubmit={addNewCar}>
-            {preInput('Title', 'Title and model of your car. Should be Short and descriptive')}
-            <input type='text' placeholder='Car Name & Brand' value={title} onChange={e => setTitle(e.target.value)} />
-            {preInput('Address', 'Address to your place')}
-            <input type="text" placeholder='Address' value={address} onChange={e => setAddress(e.target.value)} />
-            {preInput('Contact', 'Your Mobile Phone Number')}
-            <input type="text" placeholder='Phone number, +254...' value={contaact} onChange={e => setContaact(e.target.value)} />
-            {preInput('Photos', 'Upload descriptive  photos kindly')}
-            <PhotoUploader addedPhotos={addedPhotos} onChange={setAddedPhotos} />
-            {preInput('Description', 'Provide More Info about this car. Be detailed as possible')}
-            <textarea value={description} onChange={e => setDescription(e.target.value)}/>
-            {preInput('Perks', 'Select all that apply')}
-            <div className='grid grid-cols-2 mt-2 gap-2 md:grid-cols-3 lg:grid-cols-6'>
-             <Perks selected={perks} onChange={setPerks} />
-            </div>
-            {preInput('Extra Info', 'Car rules and any other vital information')}
-            <textarea value={extraInfo} onChange={e => setExtraInfo(e.target.value)} />
-            {preInput('Number of Days', 'How many days can your car be rented? Max number of passengers your car can accommodate')}
-            <div className='grid gap-2 sm:grid-cols-3'>
-              <div>
-                <h3 className='mt-2 -mb-2'>First day available</h3>
-                <input type="text" placeholder='31/03/2023 19:00...' value={checkin} onChange={e => setCheckin(e.target.value)} />
-              </div>
-              <div>
-                <h3 className='mt-2 -mb-1'>Last day/Return date</h3>
-                <input type="text" placeholder='31/03/2023 19:00...' value={checkout} onChange={e => setCheckout(e.target.value)} />
-              </div>
-              <div>
-                <h3 className='mt-2 -mb-1'>Number of passengers</h3>
-                <input type="text" placeholder='Passengers' value={maxPass} onChange={e => setMaxPass(e.target.value)} />
-              </div>
-            </div>
-            <button className='primary my-4'>Save</button>
-          </form>
-        </div>
+        <NewCarForm />
     </div>
   )
 }
--- client/src/pages/NewCarForm.jsx
+++ client/src/pages/NewCarForm.jsx
@@ -1,90 +1,96 @@
 import axios from 'axios';
 import React, { useState } from 'react'
-import { useParams } from 'react-router-dom';
-import Account from './Account'
+import { useNavigate } from 'react-router-dom';
+import Perks from '../Perks';
+import PhotoUploader from './PhotoUploader';
 
 const NewCarForm = () => {
-    const {id} = useParams();
-    const [title,setTitle] = useState('');
-    const [address,setAddress] = useState('');
-    const [addedPhotos,setAddedPhotos] = useState([]);
-    const [description,setDescription] = useState('');
-    const [perks,setPerks] = useState([]);
-    const [extraInfo,setExtraInfo] = useState('');
-    const [checkIn,setCheckIn] = useState('');
-    const [checkOut,setCheckOut] = useState('');
-    const [maxGuests,setMaxGuests] = useState(1);
-    const [price,setPrice] = useState(100);
-    const [redirect,setRedirect] = useState(false);
+  const [title, setTitle] = useState('');
+    const [address, setAddress] = useState('');
+    const [contaact, setContaact] = useState('');
+    const [addedPhotos, setAddedPhotos] = useState([]);
+    const [description, setDescription] = useState('');
+    const [perks, setPerks] = useState([]);
+    const [extraInfo, setExtraInfo] = useState('');
+    const [checkin, setCheckin] = useState('');
+    const [checkout, setCheckout] = useState('');
+    const [maxPass, setMaxPass] = useState(1);
 
-    async function savePlace(ev) {
-        ev.preventDefault();
-        const placeData = {
-          title, address, addedPhotos,
-          description, perks, extraInfo,
-          checkIn, checkOut, maxGuests, price,
-        };
-        if (id) {
-          // update
-          await axios.put('/places', {
-            id, ...placeData
-          });
-          setRedirect(true);
-        } else {
-          // new place
-          await axios.post('/places', placeData);
-          setRedirect(true);
-        }
-    
-      }
+    const navigate = useNavigate();
+
+    function inputHeader(text) {
+      return (
+        <h2 className="text-2xl mt-4">{text}</h2>
+      );
+    }
+    function inputDescription(text) {
+      return (
+        <p className="text-gray-500 text-sm">{text}</p>
+      );
+    }
+    function preInput(header,description) {
+      return (
+        <>
+          {inputHeader(header)}
+          {inputDescription(description)}
+        </>
+      );
+    }
+
+    async function addNewCar(e){
+      e.preventDefault();
+
+      const carData = {
+        title, address, contaact,
+        addedPhotos, description, perks, 
+        extraInfo, checkin, checkout, 
+        maxPass
+      };
+      await axios.post('/cars', {
+        title, address, contaact,
+        addedPhotos, description, perks, 
+        extraInfo, checkin, checkout, 
+        maxPass
+      });
+      navigate('/account')
+    }
 
 
   return (
-<div>
-      <Account />
-      <form onSubmit={savePlace}>
-        {/* {preInput('Title', 'Title for your place. should be short and catchy as in advertisement')} */}
-        <input type="text" value={title} onChange={ev => setTitle(ev.target.value)} placeholder="title, for example: My lovely apt"/>
-        {/* {preInput('Address', 'Address to this place')} */}
-        <input type="text" value={address} onChange={ev => setAddress(ev.target.value)}placeholder="address"/>
-        {/* {preInput('Photos','more = better')} */}
-        {/* <PhotosUploader addedPhotos={addedPhotos} onChange={setAddedPhotos} /> */}
-        {/* {preInput('Description','description of the place')} */}
-        <textarea value={description} onChange={ev => setDescription(ev.target.value)} />
-        {/* {preInput('Perks','select all the perks of your place')} */}
-        <div className="grid mt-2 gap-2 grid-cols-2 md:grid-cols-3 lg:grid-cols-6">
-          {/* <Perks selected={perks} onChange={setPerks} /> */}
+    <div>
+      <form onSubmit={addNewCar}>
+        {preInput('Title', 'Title and model of your car. Should be Short and descriptive')}
+        <input type='text' placeholder='Car Name & Brand' value={title} onChange={e => setTitle(e.target.value)} />
+        {preInput('Address', 'Address to your place')}
+        <input type="text" placeholder='Address' value={address} onChange={e => setAddress(e.target.value)} />
+        {preInput('Contact', 'Your Mobile Phone Number')}
+        <input type="text" placeholder='Phone number, +254...' value={contaact} onChange={e => setContaact(e.target.value)} />
+        {preInput('Photos', 'Upload descriptive  photos kindly')}
+        <PhotoUploader addedPhotos={addedPhotos} onChange={setAddedPhotos} />
+        {preInput('Description', 'Provide More Info about this car. Be detailed as possible')}
+        <textarea value={description} onChange={e => setDescription(e.target.value)}/>
+        {preInput('Perks', 'Select all that apply')}
+        <div className='grid grid-cols-2 mt-2 gap-2 md:grid-cols-3 lg:grid-cols-6'>
+         <Perks selected={perks} onChange={setPerks} />
         </div>
-        {/* {preInput('Extra info','house rules, etc')} */}
-        <textarea value={extraInfo} onChange={ev => setExtraInfo(ev.target.value)} />
-        {/* {preInput('Check in&out times','add check in and out times, remember to have some time window for cleaning the room between guests')} */}
-        <div className="grid gap-2 grid-cols-2 md:grid-cols-4">
+        {preInput('Extra Info', 'Car rules and any other vital information')}
+        <textarea value={extraInfo} onChange={e => setExtraInfo(e.target.value)} />
+        {preInput('Number of Days', 'How many days can your car be rented? Max number of passengers your car can accommodate')}
+        <div className='grid gap-2 sm:grid-cols-3'>
           <div>
-            <h3 className="mt-2 -mb-1">Check in time</h3>
-            <input type="text"
-                   value={checkIn}
-                   onChange={ev => setCheckIn(ev.target.value)}
-                   placeholder="14"/>
+            <h3 className='mt-2 -mb-2'>First day available</h3>
+            <input type="text" placeholder='31/03/2023 19:00...' value={checkin} onChange={e => setCheckin(e.target.value)} />
           </div>
           <div>
-            <h3 className="mt-2 -mb-1">Check out time</h3>
-            <input type="text"
-                   value={checkOut}
-                   onChange={ev => setCheckOut(ev.target.value)}
-                   placeholder="11" />
+            <h3 className='mt-2 -mb-1'>Last day/Return date</h3>
+            <input type="text" placeholder='31/03/2023 19:00...' value={checkout} onChange={e => setCheckout(e.target.value)} />
           </div>
           <div>
-            <h3 className="mt-2 -mb-1">Max number of guests</h3>
-            <input type="number" value={maxGuests}
-                   onChange={ev => setMaxGuests(ev.target.value)}/>
-          </div>
-          <div>
-            <h3 className="mt-2 -mb-1">Price per night</h3>
-            <input type="number" value={price}
-                   onChange={ev => setPrice(ev.target.value)}/>
+            <h3 className='mt-2 -mb-1'>Number of passengers</h3>
+            <input type="text" placeholder='Passengers' value={maxPass} onChange={e => setMaxPass(e.target.value)} />
           </div>
         </div>
-        <button className="primary my-4">Save</button>
+        <button className='primary my-4'>Save</button>
       </form>
     </div>
   )
