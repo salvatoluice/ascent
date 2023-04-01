6b83c5aa327da9899398810ec06daf082e5172e6
salvatoluice <salvatoluice5@gmail.com>
2023-04-01 23:54:09 +0300

    Booking form refactor

--- client/src/pages/SingleCar.jsx
+++ client/src/pages/SingleCar.jsx
@@ -66,36 +66,41 @@ const SingleCar = () => {
     if (showForm) {
         return (
             <div className="book">
-            <h1 className='text-center font-bold text-2xl'>Book This Car </h1>
-            <p className='text-center italic'>{car.title}</p>
-            <div className="grid py-3 px-4">
-                <label className='font-semibold'>Full Name:</label>
-                <input type="text" placeholder='E.g John Doe' value={name} onChange={e => setName(e.target.value)} />
+                <button onClick={() => setShowForm(false)} className='rounded-full p-1 border border-black bg-white mx-4'>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+                        <path fillRule="evenodd" d="M5.47 5.47a.75.75 0 011.06 0L12 10.94l5.47-5.47a.75.75 0 111.06 1.06L13.06 12l5.47 5.47a.75.75 0 11-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 01-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 010-1.06z" clipRule="evenodd" />
+                    </svg>
+                </button>
+                <h1 className='text-center font-bold text-2xl'>Book This Car </h1>
+                <p className='text-center italic'>{car.title}</p>
+                <div className="grid py-3 px-4">
+                    <label className='font-semibold'>Full Name:</label>
+                    <input type="text" placeholder='E.g John Doe' value={name} onChange={e => setName(e.target.value)} />
+                </div>
+                <div className="grid py-3 px-4">
+                    <label className='font-semibold'>Phone Number:</label>
+                    <input type="number" placeholder='07XXXXXXXX' value={number} onChange={e => setNumber(e.target.value)} />
+                </div>
+                <div className="grid py-3 px-4">
+                    <label className='font-semibold'>Email Address:</label>
+                    <input type="email" placeholder='example@gmail.com' value={email} onChange={e => setEmail(e.target.value)} />
+                </div>
+                <div className="grid py-3 px-4">
+                    <label className='font-semibold'>Pick up date:</label>
+                    <input type="date" required value={checkin} onChange={e => setCheckin(e.target.value)} />
+                </div>
+                <div className="grid py-3 px-4">
+                    <label className='font-semibold'>Return date:</label>
+                    <input type="date" required value={checkout} onChange={e => setCheckout(e.target.value)} />
+                </div>
+                <button onClick={bookThisPlace} className='primary'>
+                    Reserve Now {numberOfDays > 0 && (
+                        <span>
+                            @Ksh. {numberOfDays * car.dayprice}
+                        </span>
+                    )} 
+                </button>
             </div>
-            <div className="grid py-3 px-4">
-                <label className='font-semibold'>Phone Number:</label>
-                <input type="number" placeholder='07XXXXXXXX' value={number} onChange={e => setNumber(e.target.value)} />
-            </div>
-            <div className="grid py-3 px-4">
-                <label className='font-semibold'>Email Address:</label>
-                <input type="email" placeholder='example@gmail.com' value={email} onChange={e => setEmail(e.target.value)} />
-            </div>
-            <div className="grid py-3 px-4">
-                <label className='font-semibold'>Pick up date:</label>
-                <input type="date" required value={checkin} onChange={e => setCheckin(e.target.value)} />
-            </div>
-            <div className="grid py-3 px-4">
-                <label className='font-semibold'>Return date:</label>
-                <input type="date" required value={checkout} onChange={e => setCheckout(e.target.value)} />
-            </div>
-            <button onClick={bookThisPlace} className='primary'>
-                Reserve Now {numberOfDays > 0 && (
-                    <span>
-                        @Ksh. {numberOfDays * car.dayprice}
-                    </span>
-                )} 
-            </button>
-        </div>
         )
     }
 
