13de65bed4c679b15094f9f5066b1a0e9c4d1c90
salvatoluice <salvatoluice5@gmail.com>
2023-04-01 17:52:43 +0300

    Booking form

--- client/src/pages/BookingForm.jsx
+++ client/src/pages/BookingForm.jsx
@@ -3,20 +3,35 @@ import React, { useState } from 'react'
 const BookingForm = () => {
     const [checkin, setCheckin] = useState('');
     const [checkout, setCheckout] = useState('');
+    const [name, setName] = useState('');
+    const [number, setNumber] = useState('');
+    const [email, setEmail] = useState('');
   return (
-    <div className='border rounded-xl mt-4 p-4'>
-        <div className="">
+    <div className='rounded-xl mt-4 p-4'>
+        <form className="book">
             <h1 className='text-center font-bold text-2xl'>Book This Car </h1>
-            <div className="block py-3 px-4">
-                <label>Pick up date:</label>
+            <div className="grid py-3 px-4">
+                <label className='font-semibold'>Full Name:</label>
+                <input type="text" placeholder='E.g John Doe' value={name} onChange={e => setName(e.target.value)} />
+            </div>
+            <div className="grid py-3 px-4">
+                <label className='font-semibold'>Phone Number:</label>
+                <input type="number" placeholder='07...' value={number} onChange={e => setNumber(e.target.value)} />
+            </div>
+            <div className="grid py-3 px-4">
+                <label className='font-semibold'>Email Address:</label>
+                <input type="email" placeholder='example@gmail.com' value={email} onChange={e => setEmail(e.target.value)} />
+            </div>
+            <div className="grid py-3 px-4">
+                <label className='font-semibold'>Pick up date:</label>
                 <input type="date" value={checkin} onChange={e => setCheckin(e.target.value)} />
             </div>
-            <div className="py-3 px-4">
-                <label>Return date:</label>
+            <div className="grid py-3 px-4">
+                <label className='font-semibold'>Return date:</label>
                 <input type="date" value={checkout} onChange={e => setCheckout(e.target.value)} />
             </div>
-            <button className='primary'>Book Now</button>
-        </div>
+            <button className='primary'>Reserve Now</button>
+        </form>
     </div>
   )
 }
