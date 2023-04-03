57ee4f7ac4de21f02714f6a2d6aec83887963a84
salvatoluice <salvatoluice5@gmail.com>
2023-04-03 23:29:50 +0300

    Single booking

--- client/src/pages/SingleBooking.jsx
+++ client/src/pages/SingleBooking.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
+import axios from 'axios'
 
 const SingleBooking = () => {
+  const [booking, setBooking] = useState(null);
     const {id} = useParams();
+    useEffect(() => {
+      if (id) {
+        axios.get('/booking').then(response => {
+          const foundBooking = response.data.find(({_id}) => _id === id);
+          if (foundBooking) {
+            setBooking(foundBooking);
+          }
+        })
+      }
+    }, []);
+
+    if (!booking) {
+      return '';
+    }
   return (
     <div>
       Single Booking here: {id}
