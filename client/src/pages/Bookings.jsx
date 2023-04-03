b2c686fd33af35321f37a1a666c5cd831a2580e9
salvatoluice <salvatoluice5@gmail.com>
2023-04-03 23:18:43 +0300

    Bookings page done

--- client/src/pages/Bookings.jsx
+++ client/src/pages/Bookings.jsx
@@ -3,6 +3,7 @@ import AccountNav from './AccountNav'
 import axios from 'axios'
 import CarImg from './CarImg';
 import {differenceInCalendarDays, format} from "date-fns";
+import { Link } from 'react-router-dom';
 
 const Bookings = () => {
   const [bookings, setBookings] = useState([]);
@@ -16,21 +17,21 @@ const Bookings = () => {
       <AccountNav />
       <div>
         {bookings?.length > 0 && bookings.map(booking => (
-          <div className='flex gap-4 bg-gray-200 rounded-xl overflow-hidden'>
+          <Link to={`/account/bookings/${booking._id}`} className='flex gap-4 bg-gray-200 rounded-xl overflow-hidden'>
             <div className='w-48'>
               <CarImg car={booking.car} />
             </div>
-            <div className='p-4'>
+            <div className='py-3 pr-2 pb-0 grow'>
               <h2 className='text-xl font-semibold'>{booking.car.title}</h2>
-              <div className='border-t border-gray-400'>
-                {format(new Date(booking.checkin), 'yyyy-MM-dd')} - {format(new Date(booking.checkout), 'yyyy-MM-dd')}
+              <div className='border-t border-gray-500 mt-1'>
+                {format(new Date(booking.checkin), 'yyyy-MM-dd')} &rarr; {format(new Date(booking.checkout), 'yyyy-MM-dd')}
               </div>
-              <div>
-                Number of days: {differenceInCalendarDays(new Date(booking.checkout), new Date(booking.checkin))} <br />
-               Price: Ksh. {booking.price}
+              <div className='text-xl'>
+              For <span className='font-bold'>{differenceInCalendarDays(new Date(booking.checkout), new Date(booking.checkin))}</span> days ||
+               Price: <span className='font-semibold'>Ksh. {booking.price}</span>
               </div>
             </div>
-          </div>
+          </Link>
         ))}
       </div>
     </div>
