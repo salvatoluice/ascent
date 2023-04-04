aa9852df6a2aa1af98c3059a081f2710295e660f
salvatoluice <salvatoluice5@gmail.com>
2023-04-04 20:40:57 +0300

    Wrap up before deployment to production!

--- client/src/pages/SingleBooking.jsx
+++ client/src/pages/SingleBooking.jsx
@@ -3,6 +3,7 @@ import { useParams } from 'react-router-dom'
 import axios from 'axios'
 import AddressLink from './AddressLink';
 import CarGallery from './CarGallery';
+import { format } from 'date-fns';
 
 const SingleBooking = ({car}) => {
   const [booking, setBooking] = useState(null);
@@ -25,9 +26,14 @@ const SingleBooking = ({car}) => {
     <div className='my-8'>
       <h1 className='text-2xl'>{booking.car.title}</h1>
       <AddressLink className="my-2 block">{booking.car.address}</AddressLink>
-      <div className="bg-gray-200 p-4 mb-4 rounded-xl">
-        <h2 className='text-xl'>Your booking information</h2>
-        dates
+      <div className="bg-gray-200 p-6 my-6 rounded-xl flex justify-between">
+        <div>
+          <h2 className='text-xl mb-2 font-semibold'>Your booking information</h2>
+          From <span className='font-bold'>{format(new Date(booking.checkin), 'yyyy-MM-dd')}</span> to <span className="font-bold">{format(new Date(booking.checkout), 'yyyy-MM-dd')}</span>
+        </div>
+        <div className='bg-primary text-white p-4 rounded-xl'>
+          Price: <span className="font-semibold">Ksh. {booking.price}</span>
+        </div>
       </div>
       <CarGallery car={booking.car} />
     </div>
