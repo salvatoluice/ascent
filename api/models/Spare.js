028cbb1bd23f7ffab8cc629aa57f26160e91b427
salvatoluice <salvatoluice5@gmail.com>
2023-04-06 11:38:39 +0300

    Add Spares Model

---
+++ api/models/Spare.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const spareSchema = new mongoose.Schema({
+    title: String,
+    type: String,
+    category: String,
+    manufacturer: String,
+    description: String,
+    supplier: String,
+    feature: String,
+    price: Number,
+    image: Number,
+});
+
+const SpareSchema = mongoose.model('Spare', spareSchema);
+
+module.exports = SpareSchema;
