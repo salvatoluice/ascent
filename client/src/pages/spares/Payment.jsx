feef1af7413a51f4b692abe40bcb504260d3bae3
salvatoluice <salvatoluice5@gmail.com>
2023-04-17 22:23:07 +0300

    Debug

--- client/src/pages/spares/Payment.jsx
+++ client/src/pages/spares/Payment.jsx
@@ -5,7 +5,7 @@ const Payment = () => {
     <div>
       <form className='book'>
         <h3 className='text-xl text-center font-semibold'>Make an Oder now</h3>
-        <span className="italic text-sm">Phone payments are only made through safaricom lines. We apoloogise for any inconveniences caused.</span>
+        <span className="italic text-sm">Phone payments are only made through safaricom lines. We apologise for any inconveniences caused.</span>
             <div className="grid py-3 px-4">
                 <label className='font-semibold'>Phone Number:</label>
                 <input type="number" placeholder='Phone Number to make payment...' />
