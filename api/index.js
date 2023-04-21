0bfb0a1a4b49e9a21bcbbe626707ebde53cd34b6
salvatoluice <salvatoluice5@gmail.com>
2023-04-21 23:03:03 +0300

    Fix bugs

--- api/index.js
+++ api/index.js
@@ -23,7 +23,7 @@ app.use(cookieParser());
 app.use('/uploads', express.static(__dirname+'/uploads'));
 app.use(cors({
   credentials: true,
-  origin: 'http://127.0.0.1:5173',
+  origin: 'http://localhost:5173/',
 }));
 
 function getUserDataFromToken(req) {
